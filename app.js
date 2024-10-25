91ac030bf5ac937cfece4cdd65cc5eba8eeb0bfb
omkar <omkar.konda@gmail.com>
2024-10-25 18:32:02 +0530

    book appt flow created

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 angular.module("myApp", []);
 
-angular.module("myApp").controller("myController", function($scope) {
+angular.module("myApp").controller("myController", function($scope, $compile, ContentService, DateService) {
     
     $scope.myData = {
         "appointments": {
@@ -67,22 +67,209 @@ angular.module("myApp").controller("myController", function($scope) {
           $scope.isDialogOpen = false;
       };
 
+      $scope.baseUrl = window.location.origin;
+
+      $scope.replaceContent = function(elementId, contentUrl){
+        
+        let url = `${$scope.baseUrl}/${contentUrl}`
+        let targetElement = document.getElementById(elementId);
+
+        ContentService.getExternalContent(url).then(function(response) {
+
+            let compiledContent =  $compile(response.data)($scope);
+            
+            angular.element(targetElement).empty();
+            angular.element(targetElement).append(compiledContent);        
+
+        }).catch(function(error) {
+            console.error('Error loading content:', error);
+        });
+      }
+
+      // Book appointment tabs
+      $scope.tabs = [
+        {
+            title: 'service',
+            icon: 'ri-service-line'
+            
+        },
+        {
+            title: 'visit type',
+            icon: 'ri-video-on-line'
+            
+        },
+        {
+            title: 'facility',
+            icon: 'ri-building-line'
+            
+        },
+
+        {
+            title: 'provider',
+            icon: 'ri-stethoscope-line'
+            
+        },
+        {
+            title: 'time',
+            icon: 'ri-time-line'
+            
+        }
+    ];
+
+    // Set default selected tab
+    $scope.selectedTab = 0;
+
+    // Function to switch tabs
+    $scope.selectTab = function(index) {
+        $scope.selectedTab = index;
+    };
+
+    $scope.isEmergency = false;
+    $scope.isNotEmergency = false;
+    
+
+    $scope.emergencyCheck = function() {        
+        $scope.isEmergency = true;
+        $scope.isNotEmergency = false;
+    };
+    $scope.emergencyCheckNo = function(bool) {        
+        $scope.isEmergency = false;
+        $scope.isNotEmergency = true;
+    };
 
 
-    // $scope.getData = async function() {
-    //     try {
-    //         const response = await $http.get('data.json');
-    //         if (response && response.data) {
-    //             return response.data;
-    //         }
-    //     } catch (error) {
-    //         console.log("Error retrieving data: ", error);
-    //     }
-    // }
+    $scope.allDays = DateService.getNext60Days();
+    $scope.weekends = DateService.filterWeekends($scope.allDays);
 
-    //  $scope.getData().then(function(data) {        
-    //     $scope.myData = data;              
-    //     $scope.$apply();
-    //   });
-         
+    $scope.isFirstDayofTheMonth = function(day) {
+        
+        return day.date === 1;
+    };
+    
+    $scope.selectDate = function(day) {
+        $scope.isDateSelected = true;
+        $scope.selctedDay = day;        
+    };
+
+
+    $scope.newAppt = {
+        service: "",
+        visitType: "",
+        facility: "",
+        provider: "",
+        date: "",
+        month: "",
+        year: "",
+        time: ""
+    }
+
+
+    $scope.service = "";
+    $scope.visitType = "";
+    $scope.facility = "";
+    $scope.provider = "";
+    $scope.time = "";
+
+    $scope.selectService = function(service) {
+        $scope.service = service;
+        $scope.selectTab(1);
+    };
+    $scope.selectVisitType = function(visitType) {
+        $scope.visitType = visitType;
+        $scope.selectTab(2);
+    };
+    $scope.selectFacility = function(facility) {
+        $scope.facility = facility;
+        $scope.selectTab(3);
+    };
+    $scope.selectProvider = function(provider) {
+        $scope.provider = provider;
+        $scope.selectTab(4);
+    };
+
+    $scope.selectTime = function(time) {
+        $scope.time = time;
+    };
+
+    $scope.submitAppt = function() {
+        $scope.newAppt.service = $scope.service;
+        $scope.newAppt.visitType = $scope.visitType;
+        $scope.newAppt.facility = $scope.facility;
+        $scope.newAppt.provider = $scope.provider;
+        $scope.newAppt.date = $scope.selctedDay.date;
+        $scope.newAppt.month = $scope.selctedDay.month;
+        $scope.newAppt.year = $scope.selctedDay.year;
+        $scope.newAppt.time = $scope.time;
+
+    }
+
+    $scope.modifyAppt = function() {
+        $scope.newAppt.service = "";
+        $scope.newAppt.visitType = "";
+        $scope.newAppt.facility = "";
+        $scope.newAppt.provider = "";
+        $scope.newAppt.date = "";
+        $scope.newAppt.month = "";
+        $scope.newAppt.year = "";
+        $scope.newAppt.time = "";
+        $scope.selectTab(0);
+    }
+
+
+
 });
+
+angular.module('myApp').service('ContentService', ['$http', function($http) {    
+    this.getExternalContent = function(url) {        
+        return $http.get(`${url}`);
+    };
+}]);
+
+
+angular.module('myApp').service('DateService', function() {
+    this.getNext60Days = function() {
+        const days = [];
+        const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
+                       'August', 'September', 'October', 'November', 'December'];
+        
+        const currentDate = new Date();
+        
+        for(let i = 0; i < 60; i++) {
+            const nextDate = new Date(currentDate);
+            nextDate.setDate(currentDate.getDate() + i);
+            
+            const dateObj = {
+                date: nextDate.getDate(),
+                month: months[nextDate.getMonth()],
+                year: nextDate.getFullYear(),
+                weekDay: weekDays[nextDate.getDay()],
+                shortWeekDay:weekDays[nextDate.getDay()].substring(0, 3).toUpperCase(),
+                formattedDate: nextDate.toISOString().split('T')[0],
+                dayOfYear: Math.floor((nextDate - new Date(nextDate.getFullYear(), 0, 0)) / (1000 * 60 * 60 * 24))
+            };
+            
+            days.push(dateObj);
+        }
+        
+        return days;
+    };
+    
+    this.filterWeekends = function(days) {
+        return days.filter(day => 
+            day.weekDay === 'Saturday' || day.weekDay === 'Sunday'
+        );
+    };
+
+  
+    
+    this.groupByMonth = function(days) {
+        return days.reduce((acc, day) => {
+            if (!acc[day.month]) {
+                acc[day.month] = [];
+            }
+            acc[day.month].push(day);
+            return acc;
+        }, {});
+    };
+})
